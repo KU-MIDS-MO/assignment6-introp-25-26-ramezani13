29da314e136ff2cc1f47852eab5f4326a9b69780
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2025-12-10 11:13:28 +0100

    done

--- estimate_pi.py
+++ estimate_pi.py
@@ -1,7 +1,18 @@
+import numpy as np
+
 def estimate_pi(num_samples):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    count_inside = 0
+
+    for _ in range(num_samples):
+        ## generate random point (x, y) in [0,1] × [0,1]
+        x = np.random.rand()
+        y = np.random.rand()
+
+        ## check if insid quarter circle
+        if x*x + y*y <= 1.0:
+            count_inside += 1
+
+    ## Monte Carlo estimate
+    pi_hat = 4.0 * count_inside / num_samples
+    return float(pi_hat)
--- random_unit_vectors.py
+++ random_unit_vectors.py
@@ -1,7 +1,23 @@
+import numpy as np
+
 def random_unit_vectors(num_vectors, dim):
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    M = np.random.randn(num_vectors, dim)
+
+    U = np.zeros((num_vectors, dim), dtype=float)
+
+    for i in range(num_vectors):
+
+        total = 0.0
+        for j in range(dim):
+            total += M[i, j] * M[i, j]   # sum of squares
+        l2_norm = np.sqrt(total)
+
+        if l2_norm == 0:
+            l2_norm = 1.0
+
+        for j in range(dim):
+            U[i, j] = M[i, j] / l2_norm
+
+    return U
+
